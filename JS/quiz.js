6e7043e73f9902d1a5d6930de709e910b34cb855
Gian-Luca Rausa <gianluca.rausa>
2024-12-22 13:18:10 +0100

    Taschenrechner

--- JS/quiz.js
+++ JS/quiz.js
@@ -1,22 +1,56 @@
 'use strict'
 
-const quiz1input = document.getElementById("quizinputid");
-const frage = document.getElementById("frage");
+const zahl1 = document.getElementById("zahl1");
+const zahl2 = document.getElementById("zahl2");
+const rechenart = document.getElementById("rechenart");
+const submitbutton = document.getElementById("rechnenbutton");
+const resetbutton = document.getElementById("resetbutton");
 
-const frage2 = null;
-
-function quiz1() {
-    if(quiz1input.value !="") {
-        if(quiz1input.value == 7) {
-            const confirmquiz1 = window.confirm("Die Antwort war richtig!\nMit Klicken auf Ok kommt das nächste Quiz")
-            if(confirmquiz1) {
-                quiz1input.value = "";
-                quiz1input.focus;
+function calculate() {
+    if(zahl1.value !="" && zahl2.value !="") {
+        const num1 = parseFloat(zahl1.value); 
+        const num2 = parseFloat(zahl2.value);
+        if(rechenart.value == "Addieren"){
+            const ergebnis = num1 + num2;
+            const confirmed = confirm("Das ergebnis ist:\n" + ergebnis);
+            if(confirmed) {
+                zahl1.value = "";
+                zahl2.value = "";
+                zahl1.focus();
             }
-        } else {
-            window.alert("Die Antwort war falsch!")
+        } else if(rechenart.value == "Subtrahieren") {
+            const ergebnis = num1 - num2;
+            const confirmed = confirm("Das ergebnis ist:\n" + ergebnis);
+            if(confirmed) {
+                zahl1.value = "";
+                zahl2.value = "";
+                zahl1.focus();
+            }
+        } else if(rechenart.value == "Multiplizieren") {
+            const ergebnis = num1 * num2;
+            const confirmed = confirm("Das ergebnis ist:\n" + ergebnis);
+            if(confirmed) {
+                zahl1.value = "";
+                zahl2.value = "";
+                zahl1.focus();
+            }
+        } else if(rechenart.value == "Dividieren") {
+            const ergebnis = num1 / num2;
+            const confirmed = confirm("Das ergebnis ist:\n" + ergebnis);
+            if(confirmed) {
+                zahl1.value = "";
+                zahl2.value = "";
+                zahl1.focus();
+            }
         }
-    }
+    } else if (zahl1.value =="" && zahl2.value =="") {
+        window.alert("Bitte geben sie 2 Zahlen ein!");
+        zahl1.focus();
+    }      
+}
 
-        
+function reset() {
+    zahl1.value = "";
+    zahl2.value = "";
+    zahl1.focus();
 }
